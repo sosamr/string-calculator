5a5f7b9a3a6e588c01a72fcc1bb1955bc55d1126
sosamr <marcelososa@hotmail.com>
2022-11-22 10:09:18 +1300

    Step 3 - Implemented new line as a valid separator (in addition to commas)

--- src/main/java/com/sosamr/calculator/StringCalculator.java
+++ src/main/java/com/sosamr/calculator/StringCalculator.java
@@ -1,12 +1,18 @@
 package com.sosamr.calculator;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class StringCalculator {
 
     private final static String DELIMITER = ",";
+    private final static String NEW_LINE_DELIMITER = "\n";
 
     /**
      *
-     * @param numbers - comma separated list of valid integer numbers.
+     * @param numbers - comma or new line separated list of valid integer numbers.
      *                Assumptions: Must be not null, all the values are valid integer numbers.
      * @return the sum of the numbers or 0 for an empty string
      */
@@ -15,13 +21,28 @@ public class StringCalculator {
             return 0;
         }
 
-        String[] allNumbers = numbers.split(DELIMITER);
+        checkForConsecutiveDelimiters(numbers);
 
-        int sum = 0;
-        for (String number: allNumbers) {
-            sum = sum + Integer.parseInt(number);
-        }
-        return sum;
+        List<String> allNumbers = parseNumbers(numbers);
+
+        return allNumbers.stream()
+                .map(Integer::parseInt)
+                .reduce(0, Integer::sum);
+
     }
 
+    private void checkForConsecutiveDelimiters(String input) throws IllegalArgumentException {
+        if (input.contains(DELIMITER + NEW_LINE_DELIMITER) || input.contains(NEW_LINE_DELIMITER + DELIMITER)) {
+            throw new IllegalArgumentException("Two consecutive delimiters is not allowed");
+        }
+    }
+
+    private List<String> parseNumbers(String input) {
+        final String[] lines = input.split(NEW_LINE_DELIMITER);
+
+        return Stream.of(lines)
+                .map(line -> line.split(DELIMITER))
+                .flatMap(Arrays::stream)
+                .collect(Collectors.toList());
+    }
 }
